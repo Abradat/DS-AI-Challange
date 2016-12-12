82c4522d148bc2f4ac42235c36f3b81be3901325
abradat <shayankhorsandi1376@gmail.com>
2016-12-12 18:43:59 +0330

    Initial Commit

---
+++ DS-AI-Challange/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  DS-AI-Challange
+//
+//  Created by Shayan Khorsandi on 12/12/16.
+//  Copyright © 2016 Shayan Khorsandi. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
