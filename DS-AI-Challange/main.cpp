5363a1d7a0d42b7a0fc4b51e143cc697c481339f
abradat <shayankhorsandi1376@gmail.com>
2016-12-12 18:53:15 +0330

    first commit

--- DS-AI-Challange/main.cpp
+++ DS-AI-Challange/main.cpp
@@ -1,15 +1,35 @@
-//
-//  main.cpp
-//  DS-AI-Challange
-//
-//  Created by Shayan Khorsandi on 12/12/16.
-//  Copyright © 2016 Shayan Khorsandi. All rights reserved.
-//
+/*
+ * main.cpp
+ *
+ *  Created on: Feb 6, 2016
+ *      Author: armin
+ */
 
 #include <iostream>
+#include <cstdlib>
+#include "Controller.h"
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+std::string argNames[4] = { "AICHostIP", "AICHostPort", "AICToken","AICRetryDelay" };
+std::string argDefaults[4] = { "127.0.0.1", "7099", "00000000000000000000000000000000", "1000" };
+
+int main() {
+	setbuf(stdout, NULL);
+
+	std::string args[4];
+	for (int i = 0; i < 4; i++) {
+		char* tmp = std::getenv(argNames[i].c_str());
+		if (tmp == NULL)
+			args[i] = std::string(argDefaults[i]);
+		else
+			args[i] = tmp;
+	}
+
+	try {
+		Controller *control = new Controller(args);
+		control->start();
+	} catch (std::exception& e) {
+		std::cerr << "MAIN EXCEPTION\n";
+		std::cerr << e.what() << std::endl;
+	}
+	return 0;
 }
